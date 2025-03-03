be705f6b9156ba66cbfc23c123d33aa58aeac544
CCTheGoat <carv0009@student.acitech.org>
2025-03-03 09:12:36 -0500

    just need black text

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const button1 = document.getElementById("button1");
+let text = document.getElementById("text");
+let button1 = document.getElementById("button1");
+button1.addEventListener("click", annoy);
 
-button1.addEventListener("click", yell_at_user);
+let clicks = 0;
 
-function yell_at_user() {
-    alert("STOP PRESSING THIS BUTTON");
+function annoy() {
+    clicks++;
+    if(clicks == 1) {
+        text.innerHTML = "Hey! Stop that!";
+    }
+    else if(clicks == 5) {
+        text.innerHTML = "I said stop that!";
+    }
+    else if(clicks == 10) {
+        text.innerHTML = "I will find you.";
+    }
 }
